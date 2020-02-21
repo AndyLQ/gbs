e858c2fe83c65991137413a0ec024bde5c1523e9
AndyLQ <32448344+AndyLQ@users.noreply.github.com>
2020-02-21 09:39:54 +0100

    Set up repo
    With a blank html file and a stolen manifest, service worker.

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('../sw.js')
+      .then(function() { console.log('Service Worker Registered'); });
+    }
+});
+
