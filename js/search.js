c3205938d975b838fd826334a784ea211ef3648d
Vít Brykner <johnny.brykner@gmail.com>
2020-03-07 16:42:19 +0100

    Set up basic search
    Hiding the cards which do not match the searched string.

--- js/search.js
+++ js/search.js
@@ -6,7 +6,16 @@ document.addEventListener("DOMContentLoaded", () => {
     let allHouses = [];
     document.querySelectorAll(".post").forEach(house => allHouses.push(house));
     const searchResults = allHouses.filter(house => {
-      if (house.querySelector(".post__textbox__address").innerText.toLowerCase().includes(searchInput.value.toLowerCase())) {
+      if (
+        house
+          .querySelector(".post__textbox__street")
+          .innerText.toLowerCase()
+          .includes(searchInput.value.toLowerCase()) ||
+        house
+          .querySelector(".post__textbox__city")
+          .innerText.toLowerCase()
+          .includes(searchInput.value.toLowerCase())
+      ) {
         house.classList.remove("hidden");
         return house;
       } else {
@@ -14,9 +23,13 @@ document.addEventListener("DOMContentLoaded", () => {
         return;
       }
     });
-    document.querySelector("#search__query").innerText = "You searched for '" + query + "', which returned exactly " + searchResults.length + " results!";
+    document.querySelector("#search__query").innerText =
+      "You searched for '" +
+      query +
+      "', which returned exactly " +
+      searchResults.length +
+      " results!";
   }
-
 
   document.querySelector("#search__submit").addEventListener("click", () => {
     if (searchInput.value) {
@@ -24,6 +37,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
   document.addEventListener("keydown", key => {
-    if (key.keyCode === 13 && searchInput === document.activeElement && searchInput.value) performSearch();
+    if (
+      key.keyCode === 13 &&
+      searchInput === document.activeElement &&
+      searchInput.value
+    )
+      performSearch();
   });
 });
