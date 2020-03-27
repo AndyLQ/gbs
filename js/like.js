c968ddf463376f127a321eee34554cf52a26ee13
Katarzyna Zielińska <eaakjz@students.eaaa.dk>
2020-03-27 15:58:33 +0100

    WIP

--- js/like.js
+++ js/like.js
@@ -1,14 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   let like = document.querySelector(".like-button");
   like.addEventListener("click", () => {
-    let likeButton = document.querySelector("[data-id]");
-    let houseId = likeButton.dataset.id;
+    let houseId = like.dataset.id;
+    fetch("json/houses.json")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {});
+    console.log(houseId);
     fetch("json/houses.json", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({})
+      body: JSON.stringify({
+        saved: saved
+      })
     })
       .then(response => {
         return response.json();
