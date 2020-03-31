89a3320296eb0bf1eebfde771aa6ada147940ab0
AndyLQ <32448344+AndyLQ@users.noreply.github.com>
2020-03-31 16:05:27 +0200

    Change the housepage to take content from JSON

---
+++ js/housepage.js
@@ -0,0 +1,39 @@
+let showPost;
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("json/houses.json")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      showPost = parseInt(postID);
+
+      const topPrice = document.getElementById("topPrice");
+      const topStreet = document.getElementById("street");
+      const topCity = document.getElementById("city");
+      const topSize = document.getElementById("topSize");
+      const desc = document.getElementById("desc");
+      const type = document.getElementById("type");
+      const size = document.getElementById("size");
+      const rooms = document.getElementById("rooms");
+      const price = document.getElementById("price");
+
+      var postID = parseInt(localStorage.getItem("currenthouse"));
+      showPost = parseInt(postID);
+
+      topPrice.innerHTML = data.houses[showPost].price;
+      topStreet.innerHTML = data.houses[showPost].street;
+      topCity.innerHTML = data.houses[showPost].city;
+      topSize.innerHTML = data.houses[showPost].size;
+      desc.innerHTML = data.houses[showPost].desc;
+      type.innerHTML = data.houses[showPost].type;
+      size.innerHTML = data.houses[showPost].size;
+      rooms.innerHTML = data.houses[showPost].rooms;
+      price.innerHTML = data.houses[showPost].price;
+    });
+});
+
+function getID(data_id) {
+  localStorage.setItem("currenthouse", data_id);
+}
